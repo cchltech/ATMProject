72e0761efc767b13fec08e56b3d74557d17a103a
chenguangci <1771382149@qq.com>
2018-04-18 21:46:22 +0800

    数据库表之前添加错误，现在改正，修改一点前端页面，适应低分辨率

--- sql/create.sql
+++ sql/create.sql
@@ -11,15 +11,12 @@ CREATE TABLE `table_user` (
   `error_times` int(1) NOT NULL,
   PRIMARY KEY (`card`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `table_user` (
-  `user_name` varchar(20) NOT NULL,
-  `user_password` char(6) NOT NULL,
-  `freeze_time` char(19) DEFAULT NULL,
+CREATE TABLE `table_record` (
+  `record_number` char(14) NOT NULL,
   `card` char(14) NOT NULL,
   `money` decimal(8,3) NOT NULL,
-  `sex` varchar(10) NOT NULL,
-  `age` int(3) NOT NULL,
-  `user_id` varchar(18) NOT NULL,
-  `error_times` int(1) NOT NULL,
-  PRIMARY KEY (`card`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `time` char(19) NOT NULL,
+  PRIMARY KEY (`record_number`),
+  KEY `card` (`card`),
+  CONSTRAINT `table_record_ibfk_1` FOREIGN KEY (`card`) REFERENCES `table_user` (`card`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
